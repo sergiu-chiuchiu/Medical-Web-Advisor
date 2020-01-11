416e02e21db53c8340ccdcc1e897bee82055826f
sergiu.chiuchiu <sergiu.chiuchiu@gmail.com>
2020-01-11 14:11:02 +0200

    added postgresql connection to rds instance

--- backend-med-advisor/med_app/app.py
+++ backend-med-advisor/med_app/app.py
@@ -13,4 +13,23 @@ def create_app(config_file = 'settings.py'):
     ma.init_app(app)
     db.init_app(app)
 
+    # print("2=================================")
+    # print(app.config['SQLALCHEMY_DATABASE_URI'])
+
+    @app.cli.command('resetdb')
+    def resetdb_command():
+        """Destroys and creates the database + tables."""
+
+        from sqlalchemy_utils import database_exists, create_database, drop_database
+        if database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
+            print('Deleting database.')
+            drop_database(app.config['SQLALCHEMY_DATABASE_URI'])
+        if not database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
+            print('Creating database.')
+            create_database(app.config['SQLALCHEMY_DATABASE_URI'])
+
+        print('Creating tables.')
+        db.create_all()
+        print('Shiny!')
+
     return app
--- backend-med-advisor/med_app/settings.py
+++ backend-med-advisor/med_app/settings.py
@@ -1,6 +1,49 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir=os.path.abspath(os.path.dirname(__file__))
 
-# FLASK_APP = 'main.py'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+POSTGRES_URL="postgresql-database-1.cmti0gwdpudb.eu-central-1.rds.amazonaws.com"
+POSTGRES_USER="postgres"
+POSTGRES_PW="kmadsftw"
+POSTGRES_DB="med_app_db"
+
+SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+# print("############################################3")
+# print(SQLALCHEMY_DATABASE_URI)
+
+
+SQLALCHEMY_TRACK_MODIFICATIONS=False
+# export FLASK_APP=med_app/app.py
+# export FLASK_ENV=development
+#  flask run
+# user: postgres
+# pass: kmadsftw
+# DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
